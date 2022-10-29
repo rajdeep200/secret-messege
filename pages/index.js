cc5ac8250cc0ff25fb6cfa2ed51852c804001cd2
Rajdeep Ghosh <grajdeep2000@gmail.com>
2022-10-29 07:50:30 +0530

    UI fix

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">
       <div className="h-screen flex flex-col justify-center items-center">
-        <div className="bg-white h-5/6 flex flex-col justify-center items-center mx-3 border-4 shadow-inner border-blue-700 rounded-xl">
+        <div className="bg-white flex flex-col justify-center items-center mx-3 border-4 shadow-inner border-blue-700 rounded-xl py-5">
           <div
             className="text-xl p-2 mx-2 mb-4 bg-blue-700 border-2 border-blue-600 rounded-2xl text-white"
             style={{ fontFamily: "'Fredoka One', cursive" }}
@@ -60,7 +60,7 @@ export default function Home() {
           <div>
             <button
               style={{ fontFamily: "'Roboto', sans-serif" }}
-              className="mb-4 shadow-md text-yellow-300 bg-blue-700 px-4 py-3 rounded-xl focus:bg-blue-700"
+              className="shadow-md text-yellow-300 bg-blue-700 px-4 py-3 rounded-xl focus:bg-blue-700"
               onClick={handleSubmit}
             >
               👉 Create My Link 👈
