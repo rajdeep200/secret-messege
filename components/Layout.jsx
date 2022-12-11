70d6bdf4e9374a12025f2cdb5ff81b137d7c588d
Rajdeep Ghosh <grajdeep2000@gmail.com>
2022-12-11 23:00:01 +0530

    new enhancement added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import GenerateIcon from "./GenerateIcon";
+import { useRouter } from "next/router";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
   return (
     <div>
       <div
         className="text-center text-white text-xl text-center py-2 px-2 bg-blue-700 shadow-lg"
         style={{ fontFamily: "'Courgette', cursive" }}
       >
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center" onClick={() => router.push("/")}>
           <GenerateIcon icon="message-logo-2" size="30px" />
-          <div className="ml-2">secretmessagedare.link</div>
+          <div className="ml-2">SecretMessageDare.Link</div>
         </div>
       </div>
       {children}
+      <div style={{ fontFamily: "'Varela Round', sans-serif" }} className="w-full flex justify-evenly items-center bg-blue-700 text-white text-md py-4">
+        <p className="p-2 bg-white text-blue-700 rounded-lg" onClick={() => router.push('/about')}>🤗 About Us</p>
+        <p className="p-2 bg-white text-blue-700 rounded-lg">🤙🏻 Contact Us</p>
+      </div>
     </div>
   );
 };
---
+++ pages/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div className='p-4 text-md text-gray-500' style={{ fontFamily: "'Nunito', sans-serif" }}>
+        <p className='mb-4'>Do you want your pals to express their thoughts and opinions to you in confidence?</p>
+        <p className='mb-4'>You may compliment and be complemented by your friends, family, and other close people in an interactive secret message dare game while maintaining user privacy.</p>
+        <p className='mb-4'>You can create a secret msg link that you can share with your friends to receive secret feedback for yourself</p>
+        <p className='mb-4'>You can use our platform's anonymity to send and receive anonymous texts and compliments, regardless of whether you're shy to compliment people or just generally introverted.</p>
+        <p className='mb-4'>It's critical to consider suggestions and praise from your friends and acquaintances favourably. You can share messages that you receive as a Status/Story. You can share the secret message you received by following the instructions on the share page.</p>
+        <p className='text-gray-700 font-bold'>Note: This is only a side project and not a business. I'm the only person who created and maintains the website.</p>
+    </div>
+  )
+}
+
+export default about
