f908cd60a5197f1a2e3d4ad76c7ea4bffe32f137
Rajdeep Ghosh <grajdeep2000@gmail.com>
2022-12-27 20:21:40 +0530

    Added Ad script

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,33 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>🎉Secret message dare 2022🥳</title>
-        <meta
-          name="description"
-          content="Enter your Name, Create Secret Message dare link for whatsapp, facebook and Share it with your friends. Get unknown feedback from your friends, co-workers, and Fans."
-        />
-        <link rel="icon" href="/message-favicon.ico" />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2694152104411481"
-          crossorigin="anonymous"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-2694152104411481"
+        async
+        strategy="afterInteractive"
+        onError={(e) => {
+          console.error("Script failed to load", e);
+        }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+      <Layout>
+        <Head>
+          <title>🎉Secret message dare 2022🥳</title>
+          <meta
+            name="description"
+            content="Enter your Name, Create Secret Message dare link for whatsapp, facebook and Share it with your friends. Get unknown feedback from your friends, co-workers, and Fans."
+          />
+          <link rel="icon" href="/message-favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
